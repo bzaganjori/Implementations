828e18fd0f66956387d31bdec4bfa542cda44366
BZaganjori <bzaganjori@users.noreply.github.com>
2016-06-06 11:13:59 -0400

    Create FullQueueException.java

---
+++ source/FullQueueException.java
@@ -0,0 +1,11 @@
+package source;
+
+public class FullQueueException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public FullQueueException(String message) {
+		super(message);
+	}
+
+}
