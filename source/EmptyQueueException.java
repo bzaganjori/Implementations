28a6eaff4346cd285b72a53d04d3963d89539d2e
BZaganjori <bzaganjori@users.noreply.github.com>
2016-06-06 11:13:41 -0400

    Create EmptyQueueException.java

---
+++ source/EmptyQueueException.java
@@ -0,0 +1,11 @@
+package source;
+
+public class EmptyQueueException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public EmptyQueueException(String message) {
+		super(message);
+	}
+
+}
